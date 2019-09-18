bdd650da1d59e3d2610c8d971bfd25f5f3989f3b
Colin Sidberry <colinsidberry@gmail.com>
2019-09-18 17:21:00 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,9 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(string)
-  if string = ""
-    string = "Squawk!"
-  end  
+def parrot(string = "Squawk!")
   puts string
   return string
 end
